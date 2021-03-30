5ae761b55d6f0b5102b24fceb6f7f8d5e7b1d907
gudetama <daniellewwise@gmail.com>
2021-03-29 23:15:51 -0400

    replaced setPage with react-router Link

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,19 +1,21 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 import Home from "../images/home.svg"
 import "../pages/Main.css"
 
-const Main = ({setPage}) => {
-    const onClick = (page) => {
-        setPage(page);
-    } 
+const Main = () => {
     return (
-        <div>
-           <h3><img className= "homeimg" src={Home} width="400" height="400" alt= "Home Page Image"/></h3>
-           <h4 className="header">Take care of your plants!</h4>
-           <h5 className="body">Search a plant and add it to your garden!</h5>
-           <button className= "button" onClick={() => onClick("Explore")}>Get Started</button>
-       </div>
-    )
+      <div>
+        <h3>
+          <img className="homeimg" src={Home} width="400" height="400" alt="Home Page Image" />
+        </h3>
+        <h4 className="header">Take care of your plants!</h4>
+        <h5 className="body">Search a plant and add it to your garden!</h5>
+        <Link to="/explore">
+          <button className="button">Get Started</button>
+        </Link>
+      </div>
+    );
 }
 
 export default Main
