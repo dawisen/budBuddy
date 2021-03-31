c40cb3159636525c5960708206a5e915422e5dac
gudetama <daniellewwise@gmail.com>
2021-03-30 22:15:29 -0400

    removed unused setPage props

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,11 +1,11 @@
 import PlantList from "../components/PlantList"
 import SearchBar from "../components/SearchBar"
 
-const Explore = ({plants, setPlantId, setPage, handleSearch}) => {
+const Explore = ({plants, setPlantId, handleSearch}) => {
     return (
         <div>
             <SearchBar handleSearch={handleSearch}/>
-            <PlantList plantData={plants} page="Explore" setPlantId={setPlantId} setPage={setPage}/>
+            <PlantList plantData={plants} setPlantId={setPlantId}/>
         </div>
     )
 }
