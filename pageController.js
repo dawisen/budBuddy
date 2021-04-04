29b6ac18024b87cec7964d9875c831fc409c42a1
gudetama <daniellewwise@gmail.com>
2021-04-03 20:49:17 -0400

    added func to write scraped data to data.json file

--- pageController.js
+++ pageController.js
@@ -1,4 +1,5 @@
 const pageScraper = require("./pageScraper");
+const fs = require("fs");
 async function scrapeAll(browserInstance) {
   let browser;
   try {
@@ -9,6 +10,12 @@ async function scrapeAll(browserInstance) {
     scrapedData["HistoricalFiction"] = await pageScraper.scraper(browser, "Historical Fiction");
     scrapedData["Mystery"] = await pageScraper.scraper(browser, "Mystery");
     await browser.close();
+    fs.writeFile("data.json", JSON.stringify(scrapedData), "utf8", function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("The data has been scraped and saved successfully! View it at './data.json'");
+    });
     console.log(scrapedData);
   } catch (err) {
     console.log("Could not resolve the browser instance => ", err);
